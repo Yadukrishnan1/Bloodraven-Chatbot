84fee952499d9105900bf0659aa4f2c7c826934b
Yadu Krishnan Sarathchandran <Yadukrishnan@users.noreply.huggingface.co>
2024-10-08 19:11:44 +0000

    Update src/retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -10,7 +10,7 @@ cached_retriever = None
 def setup_retriever():
     global cached_retriever
     if cached_retriever is None:
-        db = FAISS.load_local(DB_PATH, HuggingFaceEmbeddings(model_name='BAAI/bge-base-en-v1.5'))
+        db = FAISS.load_local(DB_PATH, HuggingFaceEmbeddings(model_name='BAAI/bge-base-en-v1.5'), allow_dangerous_deserialization=True)
         with open(METADATA_PATH, "rb") as f:
             chunked_docs = pickle.load(f)
         cached_retriever = db.as_retriever(search_type="similarity", search_kwargs={'k': 4})
